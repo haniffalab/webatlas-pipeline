5fb91432ced6f093c35c1d933bd5a6f94051eb7e
Tong LI <tl10@sanger.ac.uk>
2022-06-14 12:50:48 +0000

    add script for label image generation

--- bin/generate_label.py
+++ bin/generate_label.py
@@ -14,10 +14,19 @@ import scanpy as sc
 import ome_zarr
 import numpy as np
 import re
+from build_config import get_image_basic_metadata
+from skimage.draw import disk
+import tifffile as tf
 
 
-def main(stem, h5ad):
+def main(stem, xml, h5ad):
+    dimOder, channel_names, X, Y, Z, C, T = get_image_basic_metadata(xml)
+
     adata = sc.read(h5ad)
+    # print(adata)
+
+    # if multipel sections per slide -------------
+
     # sample_ids = np.unique(adata.obs[["sample"]])
     # print(sample_ids)
     # md = {}
@@ -26,8 +35,27 @@ def main(stem, h5ad):
         # md[sample_id] = adata.uns["spatial"][i]["scalefactors"]
     # adata.obs["Y"] = adata.obsm["spatial"][:, 0]
     # adata.obs["X"] = adata.obsm["spatial"][:, 1]
-    print(adata.uns["spatial"])
-    print(adata.obsm["spatial"])
+    #----------------------------------------------
+
+    print(adata.uns["spatial"][stem]["scalefactors"])
+
+    spot_diameter_fullres = adata.uns["spatial"][stem]["scalefactors"]["spot_diameter_fullres"]
+    # hires_scalef = adata.uns["spatial"][stem]["scalefactors"]["tissue_hires_scalef"]
+    spot_coords = adata.obsm["spatial"]
+    # print(spot_coords, X, Y, Z, C, T)
+    assert adata.obs.shape[0] == spot_coords.shape[0]
+    label_ids = np.arange(1, adata.obs.shape[0] + 1)
+    label_id_col_name = "label_id"
+    if label_id_col_name not in adata.obs.columns:
+        adata.obs[label_id_col_name] = label_ids
+
+    labelImg = np.zeros((int(Y), int(X)), dtype=np.uint16)
+    print(labelImg.shape)
+    print(np.max(spot_coords))
+    for spId, (y, x) in zip(adata.obs[label_id_col_name], spot_coords):
+        labelImg[disk((x, y), spot_diameter_fullres/2)] = spId
+
+    tf.imwrite(f"{stem}.tif", labelImg)
 
 
 if __name__ == "__main__":
