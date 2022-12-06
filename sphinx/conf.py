933588e95fbd8ca23728a76f499728da02bafc48
Dave Horsfall <dave@horsfall.dev>
2022-12-06 15:14:21 +0000

    update sphonx docs

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../vitessce-pipeline'))
+
+sys.path.insert(0, os.path.abspath("../vitessce-pipeline"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'vitessce-pipeline'
-copyright = '2022, Haniffa Lab'
-author = 'Haniffa Lab'
+project = "vitessce-pipeline"
+copyright = "2022, Haniffa Lab"
+author = "Haniffa Lab"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,17 +31,15 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc'
-]
+extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -48,14 +47,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-
-html_logo = 'logo.png'
-html_theme_options = {
-    'logo_only': True
-}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
