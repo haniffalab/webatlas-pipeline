d44c7cff8e1a73ba2b43b16961ef0f8d91731bd2
Dave Horsfall <dave@horsfall.dev>
2023-05-10 09:44:15 +0100

    add initial draft for visium tutorial #75

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -47,3 +47,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+
+# These folders are copied to the documentation's HTML output
+html_static_path = ["_static"]
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/custom.css",
+]
