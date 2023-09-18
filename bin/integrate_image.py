5a17bef205d2b8eb8f04d04174993a2be6308b90
Tong LI <tl10@sanger.ac.uk>
2023-09-18 13:34:42 +0000

    added zarr label processing; extracted a function for added offset to label image

--- bin/integrate_image.py
+++ bin/integrate_image.py
@@ -5,23 +5,39 @@ import os
 import tifffile as tf
 import numpy as np
 import logging
+from ome_zarr.reader import Reader
+from ome_zarr.io import parse_url
+
+
+def add_offset(label_image, offset: int):
+    max_id = np.max(label_image)
+    mask = label != 0
+    reindexed_label = (label + offset) * mask
+    reindexed_max_id = np.max(reindexed_label)
+    logging.info(max_id, reindexed_max_id)
+    return reindexed_label
 
 
 def reindex_label(label_image: str, offset: int, out_filename: str) -> None:
     label = tf.imread(label_image).astype(np.int32)
-    max_id = np.max(label)
-    mask = label != 0
-    reindexed_label = (label + offset) * mask
-    reindexed_max_id = np.max(label)
-    logging.info(max_id, reindexed_max_id)
+    reindexed_label = add_offset(label, offset)
     tf.imwrite(out_filename, reindexed_label)
 
 
-def process_image(label_image: str, **kwargs) -> None:
-    ext = os.path.splitext(label_image)[-1]
+def reindex_label_zarr(label_image_path: str, offset: int, out_filename: str) -> None:
+    reader = Reader(parse_url(str(label_image_path)))
+    nodes = list(reader())
+    label = nodes[0].data[0]
+    reindexed_label = add_offset(label, offset)
+    tf.imwrite(out_filename, reindexed_label)
+
+
+def process_image(label_image_path: str, **kwargs) -> None:
+    ext = os.path.splitext(label_image_path)[-1]
     if ext.lower() in [".tif", ".tiff"]:
         reindex_label(**kwargs)
-    # TODO: add option for zarr
+    elif ext.lower() in [".zarr"]:
+        reindex_label_zarr(**kwargs)
 
     return
 
