60d6a1a7f645d9a993506fe9ae3daea8360f3daa
Daniela Basurto-Lozada <dann.3004@gmail.com>
2023-10-26 15:13:21 +0100

    bump ome-zarr

--- bin/integrate_image.py
+++ bin/integrate_image.py
@@ -38,12 +38,12 @@ def reindex_label_zarr(label_image_path: str, offset: int, out_filename: str) ->
     label = nodes[0].data[0]
     reindexed_label = add_offset(label.squeeze(), offset)
     os.makedirs(f"{out_filename}/OME", exist_ok=True)
-    store = parse_url("./", mode="w").store
-    temp_group = zarr.group(store=store).create_group(out_filename)
-    # write_multiscale(
-    #     np.array(reindexed_label), temp_group, axes="yx"
-    # )  # this should be used, but need to bump ome-zarr version
-    write_image(np.array(reindexed_label), temp_group, axes="yx")
+    store = parse_url(out_filename, mode="w").store
+    temp_group = zarr.group(store=store)
+    write_multiscale(
+            [np.array(reindexed_label)], temp_group, axes="yx"
+    )  # this should be used, but need to bump ome-zarr version
+    # write_image(np.array(reindexed_label), temp_group, axes="yx")
     shutil.copy(
         label_image_path + "/OME/METADATA.ome.xml",
         f"{out_filename}/OME/METADATA.ome.xml",
