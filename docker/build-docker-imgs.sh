ea7119a7a1b8de772843dfe9e06e7b72e9432ffd
dannda <dann.3004@gmail.com>
2023-11-01 15:02:24 +0000

    add build_config Dockerfile and requirements.txt

--- docker/build-docker-imgs.sh
+++ docker/build-docker-imgs.sh
@@ -2,3 +2,4 @@
 VERSION=0.3.2
 
 docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline:${VERSION} -f ./Dockerfile .
+docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline-build_config:${VERSION} -f ./Dockerfile.build_config .
