c920e539139019e3e1887b36febb497efb71b009
Dave Horsfall <dave@horsfall.dev>
2023-11-07 14:52:09 +0000

    closes #114

--- envs/build-docker-imgs.sh
+++ envs/build-docker-imgs.sh
@@ -2,4 +2,4 @@
 VERSION=0.4.0
 
 docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline:${VERSION} -f ./Dockerfile .
-docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline-build_config:${VERSION} -f ./Dockerfile.build_config .
+docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline-build_config:${VERSION} -f ./build_config/Dockerfile .
--- sphinx/README.md
+++ sphinx/README.md
@@ -6,7 +6,8 @@ From the project root directory:
 python -m venv venv
 . venv/bin/activate
 pip install --upgrade pip
-pip install -r ./envs/requirements.txt 
+pip install -r ./envs/requirements.txt
+pip install -r ./envs/dev/requirements.txt
 cd sphinx/
 make html
 ```
