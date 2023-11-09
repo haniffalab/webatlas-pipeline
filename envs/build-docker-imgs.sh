29a271a0e4b686163bd434f0e826c927f32b6f8b
Dave Horsfall <dave@horsfall.dev>
2023-11-09 14:47:52 +0000

    update docker image name

--- envs/build-docker-imgs.sh
+++ envs/build-docker-imgs.sh
@@ -3,4 +3,4 @@ VERSION=0.4.1
 
 docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline:${VERSION} -f ./Dockerfile .
 cd build_config/
-docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline-build_config:${VERSION} -f ./Dockerfile .
+docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline-build-config:${VERSION} -f ./Dockerfile .
