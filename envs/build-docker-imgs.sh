382dd620e32fa7f03f3633b730c7f2e231c5df72
Dave Horsfall <dave@horsfall.dev>
2023-11-09 14:42:39 +0000

    fix issue with docker build

--- envs/build-docker-imgs.sh
+++ envs/build-docker-imgs.sh
@@ -2,4 +2,5 @@
 VERSION=0.4.0
 
 docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline:${VERSION} -f ./Dockerfile .
-docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline-build_config:${VERSION} -f ./build_config/Dockerfile .
+cd build_config/
+docker build --platform=linux/amd64 -t haniffalab/webatlas-pipeline-build_config:${VERSION} -f ./Dockerfile .
